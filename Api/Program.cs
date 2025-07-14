1b36fba5572eb26d570e76f67e919320d47b0748
Man Ioan <31920272+johnny1990@users.noreply.github.com>
2025-07-14 20:07:24 +0300

    Add project files.

---
+++ Api/Program.cs
@@ -0,0 +1,34 @@
+namespace Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
